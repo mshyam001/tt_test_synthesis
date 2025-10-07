a9ff787c2122ebe2255e023c39af0c09a449efbe
mshyam001 <33834296+mshyam001@users.noreply.github.com>
2025-10-07 14:58:32 +0200

    Update pwm.v

--- src/pwm.v
+++ src/pwm.v
@@ -1,5 +1,12 @@
 module tt_um_ms_pw (
-    input  wire        ena, rst_n
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n,    // reset_n - low to reset
     input  wire        clk,        // Clock
     input  wire        res_ni,       // Active-low reset
     input  wire [7:0]  set_thres_i,  // Asynchronous Set threshold (sampled/synced)
